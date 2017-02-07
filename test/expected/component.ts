f476e71630af1594895d1b152b541060dc9d47db
Alban Gicquel <alban.gicquel@thomsonreuters.com>
2017-02-07 17:08:56 -0500

    Bump inliner dependency to 1.0.0
    + Transformed the task to an asynchronous one, as new version of inliner returns a promise
    + Added less to the test cases

--- test/expected/component.ts
+++ test/expected/component.ts
@@ -3,7 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'foo',
     template: '<h1>Hello World</h1>',
-    styles: ['h1 {    color: #ff0000;}']
+    styles: ['h1 {    color: #ff0000;}h3 {  color: #e6e6e6;}h2 {  background-color: #008000;}']
 })
 export class ComponentX {
     constructor() { }
--- test/fixtures/component.ts
+++ test/fixtures/component.ts
@@ -3,7 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'foo',
     templateUrl: 'component.html',
-    styleUrls: ['component.css']
+    styleUrls: ['component.css','component.less']
 })
 export class ComponentX {
     constructor() { }
